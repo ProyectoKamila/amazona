fdb0b3bc2dca14dc0fb1fdf2177fad23376e4334
Jose Vivas <jose.vivas@proyectokamila.com>
2015-03-17 12:10:33 -0430

    Ultimos cambios
    sds

--- page.php
+++ page.php
@@ -1,5 +1,6 @@
 <?php get_header();
 ?>
+<div id="page">
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -8,4 +9,5 @@
         </div>
     </div>  
 </div>
+    </div>
 <?php get_footer(); ?>
---
+++ single-product.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+<div id="single-product">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <?php
+                /**
+                 * woocommerce_before_main_content hook
+                 *
+                 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+                 * @hooked woocommerce_breadcrumb - 20
+                 */
+                do_action('woocommerce_before_main_content');
+                ?>
+
+                <?php while (have_posts()) : the_post(); ?>
+
+                    <?php wc_get_template_part('content', 'single-product'); ?>
+
+                <?php endwhile; // end of the loop. ?>
+
+                <?php
+                /**
+                 * woocommerce_after_main_content hook
+                 *
+                 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+                 */
+//            do_action('woocommerce_after_main_content');
+                ?>
+
+                <?php
+                /**
+                 * woocommerce_sidebar hook
+                 *
+                 * @hooked woocommerce_get_sidebar - 10
+                 */
+//            do_action('woocommerce_sidebar');
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
